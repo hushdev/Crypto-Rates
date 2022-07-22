1d91a78c7f618a373f74138f3ff2bcd7bf70b1e0
hushdev <ildar.hushdev@gmail.com>
2022-07-22 13:32:17 +0200

    Minor commit

--- src/utils/convert-utc.js
+++ src/utils/convert-utc.js
@@ -2,9 +2,20 @@ const convertUTC = (utc) => {
   const unixEpochTimeMS = utc * 1000;
   const d = new Date(unixEpochTimeMS);
 
-  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-];
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
   const strDate = `${monthNames[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
   return strDate;
