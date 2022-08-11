382c0b0d123e09ce3f1b60992207e37a8e3622e9
hushdev <ildar.hushdev@gmail.com>
2022-08-11 14:09:49 +0200

    Pagination fix

--- src/components/Coins/components/CoinsPagination.jsx
+++ src/components/Coins/components/CoinsPagination.jsx
@@ -2,44 +2,55 @@ import React from "react";
 import styled from "styled-components";
 import Button from "../../UI/Button";
 import Loader from "../../UI/Loader";
+import { ReactComponent as ArrowSVG } from "../../../assets/svg/bold-arrow.svg";
 
 const CoinsPagination = ({ fetchPrev, fetchNext, page, isFetching }) => {
   return (
     <StyledCoinsPagination>
-      <div className="pagination">
-        <Button onClick={fetchPrev} disabled={isFetching || page < 2}>
-          {"<"}
-        </Button>
-        {isFetching && <Loader center className="loader"/> }
-        <span className={`current-page ${isFetching && 'current-page-fetching'}`}>
-          {page}
-        </span>
-        <Button onClick={fetchNext} disabled={isFetching}>
-          {">"}
-        </Button>
-      </div>
+      <Button onClick={fetchPrev} disabled={isFetching || page < 2}>
+        <ArrowSVG className="svg-prev" />
+      </Button>
+      {isFetching && <Loader center className="loader" />}
+      <span className={`current-page ${isFetching ? "current-page-fetching" : ""}`}>{page}</span>
+      <Button onClick={fetchNext} disabled={isFetching}>
+        <ArrowSVG className="svg-next" />
+      </Button>
     </StyledCoinsPagination>
   );
 };
 
 const StyledCoinsPagination = styled.div`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
-  position: relative;
-  span.current-page {
-    margin: 0 10px;
+  .current-page {
+    margin: 0 15px;
     color: var(--text-white);
     font-weight: 500;
+
     &-fetching {
       color: transparent;
     }
   }
   button {
-    padding: 5px 9.5px;
+    width: 30px;
+    height: 30px;
+    svg {
+      fill: #ffffff;
+      width: 12px;
+      height: 15px;
+    }
+    .svg-prev {
+      transform: rotate(270deg) translateY(-6px) translateX(2px);
+    }
+
+    .svg-next {
+      transform: rotate(90deg) translateY(5px) translateX(-2px);
+    }
   }
   .loader {
-    transform: scale(.5) translateX(-100%) translateY(-50%);
+    transform: scale(0.5) translateX(-100%);
   }
 `;
 
